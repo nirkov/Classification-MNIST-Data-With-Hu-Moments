d0eac93be56309b39f1917be9338e64176ede51d
nirkov <nir.kovrovski@gmial.com>
2018-08-13 01:49:52 +0300

    new

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 
 def main():
-    #
-    train_images, train_labels, test_images, test_labels = importData.load_data(data_address = 'D:/Python Projects/MNIST_With_Moments/mnist_data')
-    classifier = train_svm_model.train_model_RBF_kernel(num_train=5000, images=train_images, tag=train_labels, gamma_value=2,
-                                                        num_iteretion=-1, c_value=50, log_transform=True, RAM_size=8000)
-    prediction, labels = predict_svm_model.predict(clf=classifier, num_test=100, images=test_images, tag=test_labels)
 
-    gamma = 0.5
+    """"
+    Change the path that matches the file location on your computer to import MNIST data.
+    """
+    train_images, train_labels, test_images, test_labels = importData.load_data(data_address = 'D:/Python Projects/MNIST_With_Moments/mnist_data')
+
+
+    gamma = 0.1
     c = 1
+    max_average_gamma = 0
+    max_gamma = 0
+    max_average_c = 0
+    max_c = 0
 
     print("------------------------------ Training SVM model on MNIST data with HuMomnt only -------------------------------------")
     print()
@@ -29,77 +34,92 @@ def main():
     print("                                 Training with RBF kernel - gamma is variable")
     print("************************************************************************************************************************")
     print()
-    while(gamma < 10 ):
+
+    while(gamma < 10.2 ):
         gamma = gamma + 0.5
         print("gamma value = "+str(gamma))
-
-        gamma_variable_classifier = train_svm_model.train_model_RBF_kernel(num_train=1000, images=train_images, tag=train_labels, gamma_value=gamma,
-                                                        num_iteretion=-1, c_value=50, log_transform=True, RAM_size=8000)
-        prediction, labels = predict_svm_model.predict(clf=gamma_variable_classifier, num_test=1000, images=test_images,tag=test_labels)
+        # make svm model's classifier. Algorithm training is done using the features obtained from calculating humoment
+        # images.
+        gamma_variable_classifier = train_svm_model.train_model_RBF_kernel(num_train=30000, images=train_images, tag=train_labels, gamma_value=gamma,
+                                                        num_iteretion=-1, c_value=80, log_transform=True, RAM_size=8000)
+        # Predicting the test data.
+        prediction, labels = predict_svm_model.predict(clf=gamma_variable_classifier, num_test=3000, images=test_images,tag=test_labels)
+        # Print statistic using 'classification_report' function of sklearn library
         statistic_printer.print_SVM_HuMoment_statistic(labels, prediction, gamma_variable_classifier)
-        statistic_printer.save_SVM_HuMoment_csvFile(labels, prediction, "gamma - variable",'gamma', gamma)
-        print("____________________________________________________________________________________________________________________")
+        # Save the calculated statistic in csv (exl) file
+        statistic_printer.save_SVM_HuMoment_csvFile(labels, prediction, "gamma - variable", 'gamma', gamma)
+        # Save the maximum accuracy and the corresponding value of gamma
+        max_average_gamma, max_gamma = statistic_printer.make_max_average(prediction, labels, 3000, max_average_gamma, max_gamma, gamma)
+        print("________________________________________________________________________________________________________")
         print()
 
-    # print()
-    #
-    # print("************************************************************************************************************************")
-    # print("                                 Training with RBF kernel - C is variable")
-    # print("************************************************************************************************************************")
-    # print()
-    # while(c < 100 ):
-    #     c = c + 5
-    #     print("C value = " + str(c))
-    #     gamma_variable_classifier = train_svm_model.train_model_RBF_kernel(num_train=10000, images=train_images, tag=train_labels, gamma_value=2,
-    #                                                     num_iteretion=-1, c_value= c, log_transform=True, RAM_size=8000)
-    #     prediction, labels = predict_svm_model.predict(clf=gamma_variable_classifier, num_test=1000, images=test_images,tag=test_labels)
-    #     statistic_printer.print_SVM_HuMoment_statistic(labels, prediction, gamma_variable_classifier)
-    #     statistic_printer.save_SVM_HuMoment_csvFile(labels, prediction, "C - variable",'C', c)
-    #     print("____________________________________________________________________________________________________________________")
-    #     print()
+    print()
+    print("max_average_gamma : "+str(max_average_gamma))
+    print("max_gamma : "+str(max_gamma))
+    print()
+    print("************************************************************************************************************************")
+    print("                                 Training with RBF kernel - C is variable")
+    print("************************************************************************************************************************")
+    print()
 
-    # print()
-    # print("************************************************************************************************************************")
-    # print("                                Training with RBF kernel - Percision(C,gamma,iteration")
-    # print("************************************************************************************************************************")
-    # print()
+    while(c < 102 ):
+        c = c + 5
+        print("C value = " + str(c))
+        # make svm model's classifier. Algorithm training is done using the features obtained from calculating humoment
+        # images.
+        gamma_variable_classifier = train_svm_model.train_model_RBF_kernel(num_train=30000, images=train_images, tag=train_labels, gamma_value=0.5,
+                                                        num_iteretion=-1, c_value= c, log_transform=True, RAM_size=8000)
+        # Predicting the test data.
+        prediction, labels = predict_svm_model.predict(clf=gamma_variable_classifier, num_test=1000, images=test_images,tag=test_labels)
+        # Print statistic using 'classification_report' function of sklearn library
+        statistic_printer.print_SVM_HuMoment_statistic(labels, prediction, gamma_variable_classifier)
+        # Save the calculated statistic in csv (exl) file
+        statistic_printer.save_SVM_HuMoment_csvFile(labels, prediction, "C - variable",'C', c)
+        # Save the maximum accuracy and the corresponding value of c
+        max_average_c, max_c = statistic_printer.make_max_average(prediction, labels, 1000, max_average_c, max_c, c)
+        print("_______________________________________________________________________________________________________")
+        print()
 
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+    print("max_average_c : " + str(max_average_c))
+    print("max_c : " + str(max_c))
+    print()
+    print()
+    print("************************************************************************************************************************")
+    print("                                Training with RBF kernel - Percision(C,gamma,iteration")
+    print("************************************************************************************************************************")
+    print()
 
-    # Make data.
+    max_percision = 0
+    gamma = 0.1
+    c = 1
     GAMMA = []
     C = []
     PERCISION = []
-    for gamma in range(1,6):
-        for c in range(1, 6):
-
-            gamma_variable_classifier = train_svm_model.train_model_RBF_kernel(num_train=1000, images=train_images,
+    while (gamma < 3.2):
+        while (c < 102 ):
+            gamma_variable_classifier = train_svm_model.train_model_RBF_kernel(num_train=15000, images=train_images,
                                                                                tag=train_labels, gamma_value=gamma,
-                                                                               num_iteretion=2000, c_value=c,
+                                                                               num_iteretion=10000, c_value=c,
                                                                                log_transform=True, RAM_size=8000)
-            prediction, labels = predict_svm_model.predict(clf=gamma_variable_classifier, num_test=1000,
+            prediction, labels = predict_svm_model.predict(clf=gamma_variable_classifier, num_test=3000,
                                                            images=test_images, tag=test_labels)
             GAMMA.append(gamma)
             C.append(c)
             avr = 0
-            for i in range (0,1000):
+            for i in range (0,3000):
                 if(prediction[i]==labels[i]):
                     avr = avr + 1
-            avr = (avr/1000)*100
+            avr = (avr/3000)*100
             PERCISION.append(avr)
+            if(max_percision < avr):
+                max_percision = avr
+            c = c + 10
+        gamma = gamma + 0.3
+        c = 1
 
-    GAMMA = np.array(GAMMA)
-    C = np.array(C)
-    PERCISION = np.array(PERCISION)
+    make_3D_graph(GAMMA,'gamma',C,'c',PERCISION,'prediction')
 
-    ax.scatter(C, GAMMA, PERCISION, c='r', marker='o')
 
-    ax.set_xlabel('c')
-    ax.set_ylabel('gamma')
-    ax.set_zlabel('percision')
-
-    plt.show()
 
 
 if __name__ == "__main__":
--- statistic_printer.py
+++ statistic_printer.py
@@ -21,7 +21,7 @@ def print_SVM_HuMoment_statistic(labels,prediction,classifier):
     print()
     print("SVM with HuMoment only on MNIST data -\nClassification report for classifier %s:\n\n%s\n"
           % (classifier, sklearn.metrics.classification_report(y_true=labels, y_pred=prediction,
-          target_names=target_names,digits=3)))
+          target_names=target_names,digits=4)))
     print("Confusion matrix: \neach row of the matrix represents the instances in a predicted class \n"
           "end each column represents the instances in an actual class. \n"
           "\n%s" % sklearn.metrics.confusion_matrix(labels, prediction))
@@ -32,9 +32,9 @@ def print_SVM_HuMoment_statistic(labels,prediction,classifier):
 def save_SVM_HuMoment_csvFile(labels, prediction,fileName,variableName, variableValue):
     svm_stat = sklearn.metrics.precision_recall_fscore_support(labels, prediction)
     out_dict = {
-         "precision": svm_stat[0].round(2)
-        ,"recall": svm_stat[1].round(2)
-        ,"f1-score": svm_stat[2].round(2)
+         "precision": svm_stat[0].round(4)
+        ,"recall": svm_stat[1].round(4)
+        ,"f1-score": svm_stat[2].round(4)
         ,"support": svm_stat[3]}
 
     out_df = pd.DataFrame.from_dict(out_dict)
@@ -56,3 +56,27 @@ def save_SVM_HuMoment_txtFile(labels, prediction):
     cm = np.array2string(sklearn.metrics.confusion_matrix(labels, prediction))
     f = open('report.txt', 'w')
     f.write('Title\n\nClassification Report\n\n{}\n\nConfusion Matrix\n\n{}\n'.format(cr, cm))
+
+
+def make_max_average(prediction, labels,num_of_test,current_average,current_var_vul,candidate_var_vul):
+    counter = 0
+    for i in range(0, num_of_test):
+        if (prediction[i] == labels[i]):
+            counter = counter + 1
+    max_average = (100 * counter) // float(num_of_test)
+    if(current_average < max_average):
+        current_average = max_average
+        current_var_vul = candidate_var_vul
+    return current_average, current_var_vul
+
+def make_3D_graph(x,x_name,y,y_name,prediction,prediction_name):
+    x = np.array(x)
+    y = np.array(y)
+    prediction = np.array(prediction)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.scatter(x, y, prediction, c='r', marker='o')
+    ax.set_xlabel(x_name)
+    ax.set_ylabel(y_name)
+    ax.set_zlabel(prediction_name)
+    plt.show()
